a4bab6963fe315a1ada3098d5d94befc6cc41b5f
mirandazellnik <mirandazellnik@gmail.com>
2025-10-28 12:42:26 -0700

    Revert tasks.py

--- backend_server/services/tasks.py
+++ backend_server/services/tasks.py
@@ -22,7 +22,7 @@ from backend_server.utils.search_by_essentials import search_by_essentials
 
 from backend_server.utils.database_tools.get_ingredients_for_label import get_ingredients_for_label
 from backend_server.utils.database_tools.get_rating import get_ratings_for_id
-from backend_server.utils.database_tools.top_by_essentials import get_top_fast
+#from backend_server.utils.database_tools.top_by_essentials import get_top_fast
 
 logger = logging.getLogger(__name__)
 
@@ -345,12 +345,12 @@ def get_products_for_essential(user_id, essential_name):
                 pass
         """
 
-        top_with_essential = search_by_essentials([essential_name], n=10)
+        #top_with_essential = search_by_essentials([essential_name], n=10)
         
-        roomName = str(user_id) + "-e_" + essential_name
-        print(roomName + "room name <---------")
+        #roomName = str(user_id) + "-e_" + essential_name
+        #print(roomName + "room name <---------")
 
-        socketio.emit("e_essential_products", {"room": roomName, "data": {"essential": essential_name, "products": top_with_essential}}, room=roomName)
+        #socketio.emit("e_essential_products", {"room": roomName, "data": {"essential": essential_name, "products": top_with_essential}}, room=roomName)
     except Exception as e:
         logger.exception("Failed to fetch products for essential %s: %s", essential_name, e)
         socketio.emit("e_essential_products_error", {"room": roomName, "data": {"essential": essential_name, "error": str(e)}}, room=roomName)
