608a498f0fbbb334cd4ceb07dbd895c0f543326c
mirandazellnik <mirandazellnik@gmail.com>
2025-10-28 23:11:23 -0700

    Fully commit new db_query method (missed file in prevcommit)

--- backend_server/utils/database_tools/db_query.py
+++ backend_server/utils/database_tools/db_query.py
@@ -1,6 +1,9 @@
 from sqlalchemy import text
 from backend_server.utils.extensions import db
 
+def db_execute_no_result(query, params=None):
+    db.session.execute(text(query), params or {})
+
 def db_execute(query, params=None):
     result = db.session.execute(text(query), params or {})
     return result.fetchall()
